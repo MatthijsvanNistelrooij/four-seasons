2857fd60e056a31d35adcec0b89e0b3fb6800517
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-03 13:12:58 +0200

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,72 +14,73 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className="flex justify-between items-center p-4 sticky top-0 bg-white text-black border-gray-700 shadow-lg z-50">
-
-      <Link href={"/"} className="text-black px-1 py-2 transition-all ml-4">
-        <Image src={image} height={100} width={400} alt="Logo" />
-      </Link>
-
-      <div className="hidden lg:flex gap-8 ml-8 items-center">
-        <Link
-          href="/"
-          className="font-big border-b-2 border-[#e9207e] text-lg font-bold"
-        >
-          Home
+    <div className=" sticky top-0 shadow-lg">
+      <div className="flex container mx-auto justify-between items-center py-4 bg-white text-black border-gray-700 z-50">
+        <Link href={"/"} className="text-black px-1 py-2 transition-all ml-4">
+          <Image src={image} height={100} width={400} alt="Logo" />
         </Link>
-        <Link href="/about" className="font-big text-lg font-bold">
-          Over
-        </Link>
-        <Link href="/" className="font-big text-lg font-bold">
-          Behandelingen
-        </Link>
-        <Link href="/" className="font-big text-lg font-bold">
-          Beauty
-        </Link>
-        <Link
-          href="/behandelingen"
-          className="border text-lg font-bold text-white bg-black rounded-full px-4 py-1"
-        >
-          Contact
-        </Link>
-      </div>
 
-      <div className="lg:hidden mr-4">
-        <button onClick={() => setIsOpen(!isOpen)} className="text-3xl">
-          ☰
-        </button>
-      </div>
-
-      <div className="hidden lg:flex flex-col mr-8">
-        <div className="font-bold text-lg">Reitemakersrijge</div>
-        <div className="flex justify-end gap-2 text-lg">
-          <span className="font-bold">3</span>
-          Groningen
-        </div>
-      </div>
-
-      {isOpen && (
-        <div className="absolute top-[100%] left-0 w-full bg-white shadow-md border-t z-40 flex flex-col items-start px-6 py-4 lg:hidden">
-          <Link href="/" className="py-2 text-lg font-bold w-full">
+        <div className="hidden lg:flex gap-8 ml-8 items-center">
+          <Link
+            href="/"
+            className="font-big border-b-2 border-[#e9207e] text-lg font-bold"
+          >
             Home
           </Link>
-          <Link href="/about" className="py-2 text-lg font-bold w-full">
+          <Link href="/about" className="font-big text-lg font-bold">
             Over
           </Link>
-          <Link href="/" className="py-2 text-lg font-bold w-full">
+          <Link href="/" className="font-big text-lg font-bold">
             Behandelingen
           </Link>
-          <Link href="/" className="py-2 text-lg font-bold w-full">
+          <Link href="/" className="font-big text-lg font-bold">
             Beauty
           </Link>
           <Link
             href="/behandelingen"
-            className="py-2 text-lg font-bold w-full text-white bg-black rounded-full text-center"
+            className="border text-lg font-bold text-white bg-black rounded-full px-4 py-1"
           >
             Contact
           </Link>
         </div>
-      )}
+
+        <div className="lg:hidden mr-4">
+          <button onClick={() => setIsOpen(!isOpen)} className="text-3xl">
+            ☰
+          </button>
+        </div>
+
+        <div className="hidden lg:flex flex-col mr-8">
+          <div className="font-bold text-lg">Reitemakersrijge</div>
+          <div className="flex justify-end gap-2 text-lg">
+            <span className="font-bold">3</span>
+            Groningen
+          </div>
+        </div>
+
+        {isOpen && (
+          <div className="absolute top-[100%] left-0 w-full bg-white shadow-md border-t z-40 flex flex-col items-start px-6 py-4 lg:hidden">
+            <Link href="/" className="py-2 text-lg font-bold w-full">
+              Home
+            </Link>
+            <Link href="/about" className="py-2 text-lg font-bold w-full">
+              Over
+            </Link>
+            <Link href="/" className="py-2 text-lg font-bold w-full">
+              Behandelingen
+            </Link>
+            <Link href="/" className="py-2 text-lg font-bold w-full">
+              Beauty
+            </Link>
+            <Link
+              href="/behandelingen"
+              className="py-2 text-lg font-bold w-full text-white bg-black rounded-full text-center"
+            >
+              Contact
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }

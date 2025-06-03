3d405a416f8ed9a26a3e39409148ff5c10fbf46e
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-03 13:19:07 +0200

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,8 +14,8 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className=" sticky top-0 shadow-lg">
-      <div className="flex container mx-auto justify-between items-center py-4 bg-white text-black border-gray-700 z-50">
+    <div className="sticky top-0 shadow-lg relative z-50">
+      <div className="flex container mx-auto justify-between items-center py-4 bg-white text-black border-gray-700">
         <Link href={"/"} className="text-black px-1 py-2 transition-all ml-4">
           <Image src={image} height={100} width={400} alt="Logo" />
         </Link>

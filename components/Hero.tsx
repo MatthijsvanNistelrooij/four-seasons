f76b2d9b1d311eada3b0b827997663d35acdff64
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-06 04:04:17 +0200

    update reviewsection

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -133,7 +133,7 @@ const Hero = ({ onOpenDialog }: HeroProps) => {
               </h1>
             </motion.div>
 
-            <div className="mt-6 md:mt-12 mb-20">
+            <div className="mt-6 md:mt-12">
               <motion.div
                 key={`contact-${index}`}
                 initial={{ opacity: 0, y: 20 }}
@@ -156,7 +156,7 @@ const Hero = ({ onOpenDialog }: HeroProps) => {
           </div>
         </div>
       </div>
-      <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 z-20 flex gap-5">
+      <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 z-20 flex gap-5 mt-10">
         {slides.map((_, i) => (
           <button
             key={i}

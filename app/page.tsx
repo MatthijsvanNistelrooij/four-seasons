11368a9ed24725ba5d9fdc565422fa9d7fb2a491
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-06 03:11:06 +0200

    added google reviews

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 "use client"
+import Image from "next/image"
+import logo_header from "../public/assets/logo_header.png"
+import shop from "../public/assets/shop.png"
+import treatment_1 from "../public/assets/treatment_1.png"
 
 import Hero from "@/components/Hero"
 import { useState } from "react"
 import { AppointmentDialog } from "@/components/AppointmentDialog"
 import GoogleReviews from "@/components/GoogleReviews"
 import Footer from "@/components/Footer"
+import { Button } from "@/components/ui/button"
+import { ArrowRight, Minus } from "lucide-react"
 
 export default function Home() {
   const [openDialog, setOpenDialog] = useState(false)
@@ -21,7 +27,7 @@ export default function Home() {
         />
       </div>
 
-      {/* <section className="w-full bg-white">
+      <section className="w-full bg-white">
         <div className="flex flex-col lg:flex-row container mx-auto items-stretch px-4 py-24 gap-12">
           <div className="w-full lg:w-1/2 h-64 lg:h-[500px]">
             <Image
@@ -29,7 +35,7 @@ export default function Home() {
               height={1200}
               src={logo_header}
               alt="Our Barbers"
-              className="w-full h-full object-cover rounded-3xl"
+              className="w-full h-full object-cover rounded-xl"
             />
           </div>
 
@@ -50,21 +56,22 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section> */}
+      </section>
 
-      {/* <section className="w-full bg-gray-200">
+      <section className="w-full bg-gray-200">
         <div className="flex flex-col lg:flex-row container mx-auto items-stretch px-4 py-24 gap-12">
-
+          {/* Image Block */}
           <div className="w-full lg:w-1/2 h-64 lg:h-[500px]">
             <Image
               src={treatment_1}
               alt="Our Barbers"
               width={1200}
               height={1200}
-              className="w-full h-full object-cover rounded-3xl"
+              className="w-full h-full object-cover rounded-xl"
             />
           </div>
 
+          {/* Text Block */}
           <div className="w-full lg:w-1/2 flex items-start">
             <div className="max-w-xl">
               <h2 className="text-3xl font-bold mb-4">
@@ -92,21 +99,22 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section> */}
+      </section>
 
-      {/* <section className="w-full bg-white">
+      <section className="w-full bg-white">
         <div className="flex flex-col lg:flex-row container mx-auto items-stretch px-4 py-24 gap-12">
-
+          {/* Image Block */}
           <div className="w-full lg:w-1/2 h-64 lg:h-[500px]">
             <Image
               width={1200}
               height={1200}
               src={shop}
               alt="Our Barbers"
-              className="w-full h-full object-cover rounded-3xl"
+              className="w-full h-full object-cover rounded-xl"
             />
           </div>
 
+          {/* Text Block */}
           <div className="w-full lg:w-1/2 flex items-start">
             <div className="max-w-xl">
               <h2 className="text-3xl font-bold mb-4">
@@ -124,67 +132,69 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section> */}
+      </section>
 
-      <section className="flex flex-col lg:flex-row items-center lg:items-start justify-center gap-14 py-48 p-4 bg-[#e9207e]">
-        <div className="w-full max-w-[600px] h-[300px] md:h-[400px] lg:h-[450px] shadow-xl">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252.76340790475925!2d6.561769514987872!3d53.21515910574965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c9cd28d615ca8b%3A0x665498987f6ab9d4!2sKapsalon%20Four%20Seasons!5e1!3m2!1sen!2snl!4v1749164707339!5m2!1sen!2snl"
-            width="100%"
-            height="100%"
-            loading="lazy"
-            className="rounded-md shadow-md"
-            referrerPolicy="no-referrer-when-downgrade"
-            allowFullScreen
-          ></iframe>
-        </div>
-
-        <div className="text-white w-full max-w-xl space-y-8">
-          <h1 className="text-3xl font-medium font-sans">
-            Openingstijden & Contact
-          </h1>
-
-          <div className="space-y-1 lg:text-xl">
-            <p>
-              <span className="font-bold">Ma</span> : 12:00 – 18:00
-            </p>
-            <p>
-              <span className="font-bold">Di</span> : 10:00 – 19:00
-            </p>
-            <p>
-              <span className="font-bold">Wo</span> : 10:00 – 19:00
-            </p>
-            <p>
-              <span className="font-bold">Do</span> : 10:00 – 20:00
-            </p>
-            <p>
-              <span className="font-bold">Vr</span> : 10:00 – 19:00
-            </p>
-            <p>
-              <span className="font-bold">Za</span> : 10:00 – 18:00
-            </p>
-            <p>
-              <span className="font-bold">Zo</span> : Gesloten
-            </p>
+      <section className="w-full bg-[#e9207e]">
+        <div className="flex flex-col lg:flex-row container mx-auto items-stretch px-4 py-24 gap-12">
+          <div className="w-full h-[600px]">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252.76340790475925!2d6.561769514987872!3d53.21515910574965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c9cd28d615ca8b%3A0x665498987f6ab9d4!2sKapsalon%20Four%20Seasons!5e1!3m2!1sen!2snl!4v1749164707339!5m2!1sen!2snl"
+              width="100%"
+              height="100%"
+              loading="lazy"
+              className="rounded-md shadow-md"
+              referrerPolicy="no-referrer-when-downgrade"
+              allowFullScreen
+            ></iframe>
           </div>
 
-          <div className="text-sm md:text-base space-y-3">
-            <p className="mb-12">
-              <strong>tel.</strong>{" "}
-              <a href="tel:0630717774" className="">
-                06 307 17774
-              </a>
-            </p>
-            <p>
-              <a href="mailto:nl.butterfly@hotmail.com" className="">
-                nl.butterfly@hotmail.com
-              </a>
-            </p>
-            <p>
-              <a href="mailto:nl.fourseasons@gmail.com" className="">
-                nl.fourseasons@gmail.com
-              </a>
-            </p>
+          <div className="text-white w-full space-y-8">
+            <h1 className="text-3xl font-medium font-sans">
+              Openingstijden & Contact
+            </h1>
+
+            <div className="space-y-1 lg:text-xl">
+              <p>
+                <span className="font-bold">Ma</span> : 12:00 – 18:00
+              </p>
+              <p>
+                <span className="font-bold">Di</span> : 10:00 – 19:00
+              </p>
+              <p>
+                <span className="font-bold">Wo</span> : 10:00 – 19:00
+              </p>
+              <p>
+                <span className="font-bold">Do</span> : 10:00 – 20:00
+              </p>
+              <p>
+                <span className="font-bold">Vr</span> : 10:00 – 19:00
+              </p>
+              <p>
+                <span className="font-bold">Za</span> : 10:00 – 18:00
+              </p>
+              <p>
+                <span className="font-bold">Zo</span> : Gesloten
+              </p>
+            </div>
+
+            <div className="text-sm md:text-base space-y-3">
+              <p className="mb-12">
+                <strong>tel.</strong>{" "}
+                <a href="tel:0630717774" className="">
+                  06 307 17774
+                </a>
+              </p>
+              <p>
+                <a href="mailto:nl.butterfly@hotmail.com" className="">
+                  nl.butterfly@hotmail.com
+                </a>
+              </p>
+              <p>
+                <a href="mailto:nl.fourseasons@gmail.com" className="">
+                  nl.fourseasons@gmail.com
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </section>
